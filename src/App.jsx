97e7661138fdc0c6d42d26fcec011c517d5b4143
Filipp Eremeev <floatfield@gmail.com>
2019-05-01 19:03:09 +0300

    use link with menu item

---
+++ src/App.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import {
+    Header,
+    Container,
+    Menu,
+    Image
+} from 'semantic-ui-react';
+
+import { TimeTable } from "./TimeTable.jsx";
+import { Tickets } from "./Tickets.jsx";
+import { About } from "./About.jsx";
+
+import { Route, Switch, Link } from "react-router-dom";
+
+export class App extends React.Component {
+    render() {           
+        return (             
+            <Container>              
+                <Menu fixed='top' inverted>
+                    <Container>                       
+                        <Menu.Item as={Link} header to="/">
+                            <Image src="./images/logo.jpg" size='mini' style={{ marginRight: '1.5em' }} />
+                             Valamis cinema
+                        </Menu.Item>                                              
+                        <Menu.Item as={Link} to="/">Time Table</Menu.Item>                       
+                        <Menu.Item as={Link} to="/about"> About</Menu.Item>                       
+                    </Container>
+                </Menu>
+                <Container text style={{ marginTop: '5em' }}>
+                    <Header as='h1' content='Valamis cinema' textAlign='center' />
+                </Container>
+                <Switch>
+                    <Route path="/" exact component={TimeTable} />
+                    <Route path="/about" exact component={About} />
+                    <Route path="/book/:id" exact component={Tickets} />
+                </Switch>  
+            </Container>
+        );
+    }
+};
--- src/index.js
+++ src/index.js
@@ -1,51 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { TimeTable } from "./TimeTable.jsx";
-import { Tickets } from "./Tickets.jsx";
-import { About } from "./About.jsx";
-import { BrowserRouter, Route, Link, Switch } from "react-router-dom";
-import {
-    Header,
-    Container,
-    Menu,
-    Image
-} from 'semantic-ui-react';
+import { App } from "./App.jsx";
+import { BrowserRouter } from "react-router-dom";
+
 import 'semantic-ui-css/semantic.min.css';
 
 "use strict";
 
 
 ReactDOM.render(
-    <BrowserRouter>        
-           
-        <Menu fixed='top' inverted>
-        <Container>
-            <Link to="/">
-                <Menu.Item as='a' header>          
-                        <Image src="./images/logo.jpg" size='mini' style={{ marginRight: '1.5em' }} />
-                        Valamis cinema                   
-                </Menu.Item> 
-            </Link>
-            <Link to="/"> 
-             <Menu.Item as='a'>Time Table</Menu.Item> 
-            </Link>
-            <Link to="/about">  
-                 <Menu.Item as='a'> About</Menu.Item>
-             </Link>
-            </Container>
-         </Menu>
-         
-        <Container  text style={{ marginTop: '5em' }}>
-                <Header as='h1' content='Valamis cinema' textAlign='center' />
-        </Container>
-
-                <Switch>
-                    <Route path="/" exact component={TimeTable} />
-                    <Route path="/about" exact component={About} />
-                    <Route exact path="/book/:id" component={Tickets}
-                    />
-                </Switch>            
-        
+    <BrowserRouter> 
+       <App/>         
     </BrowserRouter>,
     document.getElementById("root")
 );
